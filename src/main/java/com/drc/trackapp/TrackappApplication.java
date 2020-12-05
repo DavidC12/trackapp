3a42dc16292bad95605726dbb4949bf75bebe5ce
Dave Clough <10521424+DavidC12@users.noreply.github.com>
2020-12-05 13:36:59 -0500

    new project

---
+++ src/main/java/com/drc/trackapp/TrackappApplication.java
@@ -0,0 +1,13 @@
+package com.drc.trackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackappApplication.class, args);
+	}
+
+}
